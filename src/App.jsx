e82acea7e094fd4ebd8c3f3d62fb9b3bb0307c08
Danil <danilater0105@gmail.com>
2023-08-22 15:09:22 +0300

    fixed site

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 import Welkom from './Components/Welkom';
 import Home from './pages/Home';
